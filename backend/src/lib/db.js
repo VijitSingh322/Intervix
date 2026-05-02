db4427002a6e191a87ce66ec17ae5ef377edaad1
Aman Singh <aman123@gmail.com>
2026-05-02 23:18:36 +0530

    validation check added for DB_URL

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -4,7 +4,10 @@ import {ENV} from "./env.js"
 
 export const connectDB = async() => {
     try{
-    const conn =await mongoose.connect(ENV.DB_URL, {family: 4})
+    if (!ENV.DB_URL) {
+        throw new Error("DB_URL is not defined in environment variables");
+    }
+    const conn =await mongoose.connect(ENV.DB_URL)
     console.log("Connected to MongoDB:",conn.connection.host)
     } catch (error) {
         console.error("Error connecting to MongoDB",error)
