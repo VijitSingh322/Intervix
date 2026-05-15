566dbe9158bd72078c66e81b4dc682cb4ee72e32
Aman Singh <aman123@gmail.com>
2026-05-16 00:09:12 +0530

    session page added

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -3,11 +3,11 @@ import { chatClient } from "../lib/stream.js";
 export async function getStreamToken(req,res){
     try{
         //use clerId for Stream (not mongodb_id)=> it should match the id we have in the stream dashnoard
-        const token = chatClient.createToken(req.user.clerId);
+        const token = chatClient.createToken(req.user.clerkId);
 
         res.status(200).json({
             token,
-            userId: req.user.clerId,
+            userId: req.user.clerkId,
             userName: req.user.name,
             userImage: req.user.image
         });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton, useUser} from "@clerk/clerk-react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router";
-import HomePage from "./pages/HomePage";
-import ProblemsPage from "./pages/ProblemsPage";
-import DashboardPage from "./pages/DashboardPage";
-import ProblemPage from "./pages/ProblemPage";
+import { useUser } from "@clerk/clerk-react";
 import { Toaster } from "react-hot-toast";
+import { Navigate, Route, Routes } from "react-router";
+import DashboardPage from "./pages/DashboardPage";
+import HomePage from "./pages/HomePage";
+import ProblemPage from "./pages/ProblemPage";
+import ProblemsPage from "./pages/ProblemsPage";
+import SessionPage from "./pages/SessionPage";
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
         <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
         <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
         <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
+        <Route path="/session/:id" element={isSignedIn ? <SessionPage /> : <Navigate to={"/"} />} />
       </Routes>
 
       <Toaster toastOptions={{duration: 3000}}/>
